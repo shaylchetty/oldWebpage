d9ad56cdc57a79f4c18e78d4597a9ebaa6d712d7
Shayl Chetty <102982612+shaylchetty@users.noreply.github.com>
2023-02-13 22:23:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Add smooth scrolling to anchor links
+$('a[href^="#"]').on('click', function(event) {
+	event.preventDefault();
+	var target = this.hash;
+	var $target = $(target);
+
+	$('html, body').animate({
+		'scrollTop': $target.offset().top
+	}, 1000, 'swing');
+});
+
+// Toggle mobile navigation menu
+$('.toggle-menu').on('click', function() {
+	$('.menu').slideToggle();
+});
